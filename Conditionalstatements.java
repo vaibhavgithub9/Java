67bc5ef20d87f439319d1b0786c0427491184435
vaibhavgithub9 <vaibhavsai8@gmail.com>
2022-04-30 20:36:18 +0530

    Update Conditionalstatements.java

--- Conditionalstatements.java
+++ Conditionalstatements.java
@@ -9,35 +9,31 @@ int c=33;
 int d=0;
 int e=4;
 //if
-	if(a>0)
+if(a>0)
 	{System.out.println("the number is greater than 0");}
-	System.out.println("Not greater than 0");
+	 System.out.println("Not greater than 0");
 //if inside if
-	{
 	if (b>0)
 	if(b%5==0)
 	{System.out.println ("Its a multiple of 5");}
-	}
-	System.out.println("the end");
+	 System.out.println("the end");
 //if else
-	{
-		if(c>0)
-		{System.out.println("its a positive number");}
-		else
-		{System.out.println("its a negative number");}
-	}
+if(c>0)
+	{System.out.println("its a positive number");}
+else
+	{System.out.println("its a negative number");}
+	
 //else if in if else
-	{
-		if(d>0)
-		{System.out.println("its greater than zero");}
-		else if(d==0)
-		{System.out.println("its equal to zero");}
-		else
-		{System.out.println("its lesser than zero");}
-		
-	}
+if(d>0)
+	{System.out.println("its greater than zero");}
+else if(d==0)
+	{System.out.println("its equal to zero");}
+else
+	{System.out.println("its lesser than zero");}
+
 //Switch case
-	switch(e)
+
+switch(e)
 	{
 	case 1:
 	System.out.println("Case 1");
@@ -52,9 +48,8 @@ int e=4;
 	System.out.println("Case 4");
 	break;
 	default:
-		System.out.println("out of the case");
+	System.out.println("out of the case");
 	}
 	
 	}
-
 }
