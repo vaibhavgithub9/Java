6d59ede9ee55cdd47632ef5fc6722424aa32f995
vaibhavgithub9 <vaibhavsai8@gmail.com>
2022-05-01 23:59:02 +0530

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -14,45 +14,45 @@ public class Operators {
 		System.out.println(a/b);
 		System.out.println(a%b);
 // Unary operator
-	//	System.out.println(++b);
-	//	System.out.println(--a);
-	//	System.out.println(a++);
-	//	System.out.println(a);
-	//	System.out.println(b--);
-	//	System.out.println(b);
-	//	System.out.println(a+b);
-	//	System.out.println(~a);
-	//	System.out.println(!c);
+		System.out.println(++b);
+		System.out.println(--a);
+		System.out.println(a++);
+		System.out.println(a);
+		System.out.println(b--);
+		System.out.println(b);
+		System.out.println(a+b);
+		System.out.println(~a);
+		System.out.println(!c);
 // Shift operator
-	//	System.out.println(d<<2);
-	//	System.out.println(d>>2);
-	//	System.out.println(e<<2);
-	//	System.out.println(e>>2);
+		System.out.println(d<<2);
+		System.out.println(d>>2);
+		System.out.println(e<<2);
+		System.out.println(e>>2);
 // Relational operator
-	//	System.out.println(a>b);
-	//	System.out.println(a>=b);
-	//	System.out.println(a==d);
-	//	System.out.println(d<e);
+		System.out.println(a>b);
+		System.out.println(a>=b);
+		System.out.println(a==d);
+		System.out.println(d<e);
 // Assignment operator
-	//	System.out.println(f=9);
-	//	System.out.println(f+=1);
-	//	System.out.println(f*=2);
+		System.out.println(f=9);
+		System.out.println(f+=1);
+		System.out.println(f*=2);
 //Logical operator
-	//	System.out.println(a>b && b>d);
-	//	System.out.println(a>b || b<d);
+		System.out.println(a>b && b>d);
+		System.out.println(a>b || b<d);
 //Bitwise operators
-	//	System.out.println(a|b); //0101 0100= 0101=5
-	//	System.out.println(a&b); //0101 0100= 0100=4
+		System.out.println(a|b); //0101 0100= 0101=5
+		System.out.println(a&b); //0101 0100= 0100=4
 //Bitwise and logical operators &&,&
-	//	System.out.println(a<b && b++ <a); //In logical op if the first value is false the next value is not considered
-	//	System.out.println(a);
-	//	System.out.println(a<b & a++ <b); //Next value is checked if first value is false
-	//	System.out.println(a);
+		System.out.println(a<b && b++ <a); //In logical op if the first value is false the next value is not considered
+		System.out.println(a);
+		System.out.println(a<b & a++ <b); //Next value is checked if first value is false
+		System.out.println(a);
 //Bitwise and logical operator ||,|
-	//	System.out.println(a>b || b++<a);
-	//	System.out.println(a);
-	//	System.out.println(a>b | b++<a);
-	//	System.out.println(a);
+		System.out.println(a>b || b++<a);
+		System.out.println(a);
+		System.out.println(a>b | b++<a);
+		System.out.println(a);
 //Ternary operator
 		System.out.println(max);	
 	}
